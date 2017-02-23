22b7e4f2d877a6aabd3002efe5b9ac3a1b1dadef
Pedro Borges <oi@pedroborg.es>
2017-02-23 09:40:38 -0300

    Update thumbs path

--- site.php
+++ site.php
@@ -8,3 +8,5 @@ kirby()->urls()->index = url::base();
 kirby()->roots()->assets = kirby()->roots()->index() . DS . 'public';
 kirby()->urls()->assets = kirby()->urls()->index();
 
+kirby()->roots()->thumbs = kirby()->roots()->assets() . DS . 'thumbs';
+kirby()->urls()->thumbs = kirby()->urls()->assets() . '/thumbs';

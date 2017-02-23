62c8d2287536ae42ccadcda036fff86faf2c75fd
Pedro Borges <oi@pedroborg.es>
2017-02-23 09:39:09 -0300

    Update index.php paths

--- public/index.php
+++ public/index.php
@@ -3,11 +3,11 @@
 define('DS', DIRECTORY_SEPARATOR);
 
 // load kirby
-require(__DIR__ . DS . 'kirby' . DS . 'bootstrap.php');
+require(dirname(__DIR__) . DS . 'kirby' . DS . 'bootstrap.php');
 
 // check for a custom site.php
-if(file_exists(__DIR__ . DS . 'site.php')) {
-  require(__DIR__ . DS . 'site.php');
+if(file_exists(dirname(__DIR__) . DS . 'site.php')) {
+  require(dirname(__DIR__) . DS . 'site.php');
 } else {
   $kirby = kirby();
 }

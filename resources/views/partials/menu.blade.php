ee4c99a7c3969eba01b2e33254aba32417de3592
Pedro Borges <oi@pedroborg.es>
2018-07-04 15:23:03 -0300

    Update menu

--- resources/views/partials/menu.blade.php
+++ resources/views/partials/menu.blade.php
@@ -1,7 +1,13 @@
-<nav role="navigation" aria-label="site links">
-    @foreach($pages->visible() as $item)
-    <a href="{{ $item->url() }}" title="{{ $item->title() }}">
-        {{ $item->title() }}
-    </a>
-    @endforeach
+<nav role="navigation">
+    @if($pages->visible()->count())
+        <ul class="list-reset flex">
+            @foreach($pages->visible() as $item)
+            <li>
+                <a href="{{ $item->url() }}" title="{{ $item->title() }}">
+                    {{ $item->title() }}
+                </a>
+            </li>
+            @endforeach
+        </ul>
+    @endif
 </nav>
